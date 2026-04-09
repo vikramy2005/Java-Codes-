06ce4d9b01cb221cc33d69af4d19f5d540d4835d
vikramy2005 <vikramyadav5456@gmail.com>
2026-04-09 23:31:15 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,21 @@
+public class SortArray {
+    public static void main(String[] args) {
+        int arr[] = {5, 2, 9, 1, 3};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
