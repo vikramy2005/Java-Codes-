860dbdf939fb60e4f5ceaa9961282d293a9de4b1
vikramy2005 <vikramyadav5456@gmail.com>
2026-04-09 21:48:07 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,34 +3,26 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter first number: ");
         double num1 = sc.nextDouble();
-
         System.out.print("Enter second number: ");
         double num2 = sc.nextDouble();
-
         System.out.print("Enter operator (+, -, *, /): ");
         char op = sc.next().charAt(0);
-
         double result;
-
         switch (op) {
             case '+':
                 result = num1 + num2;
                 System.out.println("Result: " + result);
                 break;
-
             case '-':
                 result = num1 - num2;
                 System.out.println("Result: " + result);
                 break;
-
             case '*':
                 result = num1 * num2;
                 System.out.println("Result: " + result);
                 break;
-
             case '/':
                 if (num2 != 0) {
                     result = num1 / num2;
@@ -39,7 +31,6 @@ public class Calculator {
                     System.out.println("Cannot divide by zero");
                 }
                 break;
-
             default:
                 System.out.println("Invalid operator");
         }
