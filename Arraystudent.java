31ef779e7996b99c66484cae5a920f4e37156514
vikramy2005 <vikramyadav5456@gmail.com>
2026-04-16 10:47:40 +0530

    Add files via upload

---
+++ Arraystudent.java
@@ -0,0 +1,9 @@
+public class studentarray{
+    public static void main (String[]args){
+    Int marks [] = {20,30,50,80,90,60,70,80,90,78};
+    System.out.println("student marks:");
+    for(int i = 0; i< marks.length; i++){
+        System.out.println("student:"+(i+1)+"marks="+marks[i]);
+    }
+}
+}
